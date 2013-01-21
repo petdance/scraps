49cbf7557b7ce97c8038a22342ce4e4d6c021a23
Andy Lester <andy@petdance.com>
2013-01-21 09:29:24 -0600

    first dump

---
+++ japh/1.pl
@@ -0,0 +1,16 @@
+#!perl
+# From http://stackoverflow.com/questions/14440594/how-does-this-famous-japh-work
+not exp log srand xor s qq qx xor
+s x x length uc ord and print chr
+ord for qw q join use sub tied qx
+xor eval xor print qq q q xor int
+eval lc q m cos and print chr ord
+for qw y abs ne open tied hex exp
+ref y m xor scalar srand print qq
+q q xor int eval lc qq y sqrt cos
+and print chr ord for qw x printf
+each return local x y or print qq
+s s and eval q s undef or oct xor
+time xor ref print chr int ord lc
+foreach qw y hex alarm chdir kill
+exec return y s gt sin sort split
