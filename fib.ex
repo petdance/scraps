4f777d52b5848bc5ce7d0235f491ae203b0eb55e
Andy Lester <andy@petdance.com>
2017-04-25 14:30:18 -0500

    Errors out on negative numbers

--- fib.ex
+++ fib.ex
@@ -1,10 +1,9 @@
-
 defmodule Fib do
-    def fib(n) when n <= 2 do
+    def fib(n) when n==1 or n==2 do
         1
     end
 
-    def fib(n) do
-        fib(n-1)+fib(n-2)
+    def fib(n) when n > 0 do
+        fib(n-1) + fib(n-2)
     end
 end
