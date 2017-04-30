9de1a268777a69715e40acbcc809b66140ad23a1
Andy Lester <andy@petdance.com>
2017-04-30 16:50:39 -0500

    Use filter instead of map

--- euler/done/001.ex
+++ euler/done/001.ex
@@ -4,7 +4,7 @@ defmodule Euler001 do
   def foo do
     sum =
       1..999
-      |> Enum.map( fn(n) -> if rem(n,3) == 0 or rem(n,5) == 0, do: n, else: 0 end )
+      |> Enum.filter( fn(n) -> rem(n,3) == 0 or rem(n,5) == 0 end )
       |> Enum.sum
     IO.puts sum
   end
