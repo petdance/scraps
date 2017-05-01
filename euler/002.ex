7f1b2f397efc389dedf49ba8e7fb521779ada10c
Andy Lester <andy@petdance.com>
2017-04-30 20:40:33 -0500

    Added the summing up.

--- euler/002.ex
+++ euler/002.ex
@@ -10,14 +10,22 @@ defmodule Euler002 do
 #
 # Answer: 4613732
 
-  def fibseries(upto) do
-    fibseries( [ 1, 1 ], upto )
+  def foo( upto ) do
+    total =
+      fibseries( upto )
+      |> Enum.filter( fn(n) -> rem(n,2) == 0 end )
+      |> Enum.sum
+    IO.puts total
   end
 
-  defp fibseries( fib, upto ) do
+  defp fibseries( upto ) do
+    fibseries( upto, [ 1, 1 ] )
+  end
+
+  defp fibseries( upto, fib ) do
     new = Enum.at(fib,-1) + Enum.at(fib,-2)
     if new < upto do
-      fibseries( fib ++ [new], upto )
+      fibseries( upto, fib ++ [new] )
     else
       fib
     end
