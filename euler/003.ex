0e637c89ae95e2088edd97015697278cb1c78752
Andy Lester <andy@petdance.com>
2017-05-02 20:46:28 -0500

    Don't reuse n

--- euler/003.ex
+++ euler/003.ex
@@ -1,5 +1,4 @@
 defmodule Euler003 do
-
 # http://projecteuler.net/index.php?section=problems&id=3
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
@@ -23,12 +22,10 @@ defmodule Euler003 do
   end
 
   defp factor_down( [n | factors], test_factor ) when rem(n,test_factor) == 0 do
-    n = div(n,test_factor)
-    factor_down( [n, test_factor | factors], test_factor )
+    factor_down( [ div(n,test_factor), test_factor | factors], test_factor )
   end
 
   defp factor_down( [n | factors], test_factor ) do
     factor_down( [n | factors], test_factor+1 )
   end
-
 end
