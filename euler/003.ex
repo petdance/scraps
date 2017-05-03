60980827af60df1197392676fdc31f837f882dc0
Andy Lester <andy@petdance.com>
2017-05-02 20:34:04 -0500

    Got the factoring happy.  Now to use guards, I think.

--- euler/003.ex
+++ euler/003.ex
@@ -4,25 +4,27 @@ defmodule Euler003 do
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+  def largest_prime_factor( n ) do
+    factors = prime_factors( n )
+    [largest|_] = factors
+
+    IO.write 'Prime factors are '
+    IO.inspect factors
+    IO.write 'Largest prime factor is '
+    IO.inspect largest
+  end
+
   def prime_factors( n ) do
     factor_down( [n], 2 )
   end
 
   defp factor_down( [1 | factors], _ ) do
-    [ 1 | factors ]
+    factors
   end
 
   defp factor_down( [n | factors], potential_factor ) do
-    IO.puts ''
-    IO.puts 'into factor_down'
-    n |> IO.inspect
-    factors |> IO.inspect
-    #IO.inspect potential_factor
-
     if rem(n,potential_factor) == 0 do
       n = div(n,potential_factor)
-      IO.puts 'Got a factor'
-      IO.inspect potential_factor 
       factor_down( [n, potential_factor | factors], potential_factor )
     else
       factor_down( [n | factors], potential_factor+1 )
