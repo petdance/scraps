bf129632d153766320d59c7a0718d261c52fba36
Andy Lester <andy@petdance.com>
2017-05-11 23:02:03 -0500

    Wrote digits() functions

--- euler/004.ex
+++ euler/004.ex
@@ -1,5 +1,18 @@
-defmodule Euler003 do
+defmodule Euler004 do
   # https://projecteuler.net/problem=4
   # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
   # Find the largest palindrome made from the product of two 3-digit numbers.
+
+  def digits(n) do
+    _digits( [n] )
+  end
+
+  defp _digits( [ n | digits ] ) when n > 0 and n < 10 do
+    [ n | digits ]
+  end
+
+  defp _digits( [ n | digits ] ) when n >= 10 do
+    _digits( [ div(n,10), rem(n,10) | digits ] )
+  end
+
 end
