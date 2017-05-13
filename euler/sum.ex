7d63e5e105f7152064a50db7c64d4baa9a452043
Andy Lester <andy@petdance.com>
2017-05-13 14:03:47 -0500

    Exercises from the Elixir book

--- euler/sum.ex
+++ euler/sum.ex
@@ -20,4 +20,42 @@ defmodule Sum do
     func.(head) + _mapsum( tail, func)
   end
 
+
+  def max( list ) do
+    _max( list )
+  end
+
+  defp _max( [n] ), do: n
+
+  defp _max( [head|tail] ) do
+    Kernel.max( head, _max( tail ) )
+  end
+
+
+  def caesar( list, n ) do
+    _caesar( list, n )
+  end
+
+  defp _caesar( [], _ ), do: []
+
+  defp _caesar( [head|tail], n ) do
+    [ _rot(head,n) | _caesar( tail, n ) ]
+  end
+
+  defp _rot( c, n ) when c+n <= ?z, do: c+n
+  defp _rot( c, n ), do: c-n
+
+
+  def span( from, to ) do
+    _span( from, to )
+  end
+
+  defp _span( from, to ) when from == to do
+    [from]
+  end
+
+  defp _span( from, to ) do
+    [from | _span( from+1, to ) ]
+  end
+
 end
