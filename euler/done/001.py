d07346d0b46128d199d8725c29124a991575b9cf
Andy Lester <andy@petdance.com>
2018-10-16 23:22:55 -0500

    Fix shebang

--- euler/done/001.py
+++ euler/done/001.py
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/python
 
 """
 
@@ -11,7 +11,6 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 sum = 0
 for i in range( 0, 1000 ):
-    print i
     if i % 5 == 0 or i % 3 == 0:
         sum = sum + i
 
