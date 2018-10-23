cccb61f537695fd881bb51e1463830ceb816db40
Andy Lester <andy@petdance.com>
2018-10-22 22:06:44 -0500

    Solved

---
+++ euler/python/016.py
@@ -0,0 +1,8 @@
+# https://projecteuler.net/problem=16
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 2^1000?
+
+n = pow(2,1000)
+s = "%s" % n
+digits = [ ord(i) - ord('0') for i in list(s) ]
+print( sum( digits ) )
