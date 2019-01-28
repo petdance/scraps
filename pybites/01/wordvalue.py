8d4700c9e55a5dc3db1196e388780e4ce3cddbf6
Andy Lester <andy@petdance.com>
2019-01-27 23:43:14 -0600

    first playing

--- pybites/01/wordvalue.py
+++ pybites/01/wordvalue.py
@@ -2,11 +2,23 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    with open(DICTIONARY,'r') as f:
+        lines = [line.rstrip('\n') for line in f]
+        f.close()
 
-def calc_word_value():
+
+    return lines
+
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(LETTER_SCORES)
+
+    letters = word.split()
+    pp.pprint(word)
+    pp.pprint(letters)
     pass
 
 def max_word_value():
