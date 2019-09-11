bb5bf05a0975ca2dd3449d64a05263b762e14b11
Andy Lester <andy@petdance.com>
2019-09-11 11:49:05 -0500

    Add more cases

--- benchmarks/substr.pl
+++ benchmarks/substr.pl
@@ -8,9 +8,15 @@ use Benchmark;
 
 my $COUNT = 50_000_000;
 
-my $path = "/path/to/our/app";
+my $path_matching = "/path/to/wanted/that/is/longer";
+my $path_notmatching = "/path/to/something/else";
+my $path_very_notmatching = "bingo bongo";
 
 timethese( $COUNT, {
-    substr => sub { substr( $path, 0, 15 ) eq '/path/to/wanted' },
-    regex  => sub { $path =~ m{^/path/to/wanted} },
+    substr_match         => sub { substr( $path_matching, 0, 15 ) eq '/path/to/wanted' },
+    regex_match          => sub { $path_matching =~ m{^/path/to/wanted} },
+    substr_notmatch      => sub { substr( $path_notmatching, 0, 15 ) eq '/path/to/wanted' },
+    regex_notmatch       => sub { $path_notmatching =~ m{^/path/to/wanted} },
+    substr_very_notmatch => sub { substr( $path_very_notmatching, 0, 15 ) eq '/path/to/wanted' },
+    regex_very_notmatch  => sub { $path_very_notmatching =~ m{^/path/to/wanted} },
 } );
