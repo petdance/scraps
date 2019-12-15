dfc64edba0588334fe1e1ea7862ef84ccd379f4d
Andy Lester <andy@petdance.com>
2019-12-15 14:25:21 -0600

    Pass back line number too

--- everyother.py
+++ everyother.py
@@ -4,22 +4,26 @@
 class EveryOther:
     def __init__(self, filename):
         self.f = open(filename, 'rt')
+        self.n = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
         line = self.f.readline()
+        self.n += 1
         if line == '':
             raise StopIteration
 
         line = self.f.readline()
+        self.n += 1
         if line == '':
             raise StopIteration
 
-        return line.rstrip('\n')
+        return line.rstrip('\n'), self.n
 
 
 if __name__ == '__main__':
-    for line in EveryOther('/Users/andy/ack4/t/text/ozymandias.txt'):
-        print(line)
+    for line, n in EveryOther('/Users/andy/ack4/t/text/ozymandias.txt'):
+        print(n, line)
+        #print(str(n) + ': ' + line)
