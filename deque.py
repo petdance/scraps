c01045c7052f035db75cce494ae553c8b1e65e68
Andy Lester <andy@petdance.com>
2019-12-15 23:15:36 -0600

    playing with deque

---
+++ deque.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+lines = deque( [], 5 )
+print(lines)
+
+lines.append('a')
+print(lines)
+lines.append('b')
+lines.append('c')
+lines.append('d')
+lines.append('e')
+lines.append('f')
+lines.append('g')
+print(lines)
