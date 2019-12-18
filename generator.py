fdf3a8aef38f016edc1c5b6827378c4067ccc89c
Andy Lester <andy@petdance.com>
2019-12-17 23:11:49 -0600

    done with genreator

--- generator.py
+++ generator.py
@@ -1,19 +1,14 @@
+import sys
+
 class LineIter:
     def __init__(self, filename):
         self.f = open(filename, 'rt')
 
     def __iter__(self):
-        return self
-
-    def __next__(self):
-        line = self.f.readline()
-        if line == '':
-            raise StopIteration
-
-        line = line.upper()
-
-        return line
+        for line in self.f:
+            yield line.upper()
 
 if __name__ == '__main__':
-    for line in LineIter('wc.hs'):
-        print(line)
+    filename = sys.argv[1]
+    for line in LineIter(filename):
+        print(line, end='')
