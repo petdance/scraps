7b2e69f5fca12d2c8e51f03fa85d38d83f63e8f7
Andy Lester <andy@petdance.com>
2020-06-16 22:42:10 -0500

    First working version

---
+++ euler/rust/004.rs
@@ -0,0 +1,38 @@
+/*
+
+http://projecteuler.net/index.php?section=problems&id=4
+
+A palindromic number reads the same both ways. The largest palindrome
+made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit
+numbers.
+
+*/
+
+
+fn is_palindromic(n: u32) -> bool {
+    let s: String = n.to_string();
+
+    let rev: String = s.chars().rev().collect();
+
+    return s == rev;
+}
+
+
+fn main() {
+    let mut max: u32 = 0;
+
+    for x in 1..1000 {
+        for y in 1..1000 {
+            let n = x * y;
+            if is_palindromic(n) {
+                if n > max {
+                    max = n
+                }
+            }
+        }
+    }
+
+    println!("max = {}", max);
+}
