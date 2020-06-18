88656dac2eb6f763beb2631e8e02117c8c01c48b
Andy Lester <andy@petdance.com>
2020-06-17 22:32:31 -0500

    first working version

--- euler/rust/005.rs
+++ euler/rust/005.rs
@@ -11,7 +11,27 @@ all of the numbers from 1 to 20?
 */
 
 
+fn divisible_up_to(n: u32, max_divisor: u32) -> bool {
+    for i in 2..=max_divisor {
+        if n % i > 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 fn main() {
+    let max_divisor = 20;
+    let step = 2520;  /* We know the number has to be divisible by 210: divisible by 7, 8, 9 and 10 */
+    let mut maybe = step;
+
+    loop {
+        if divisible_up_to(maybe, max_divisor) {
+            println!("{} is divisible up to {}", maybe, max_divisor);
+            break;
+        }
+        maybe = maybe + step;  /* No point in checking the odd numbers */
+    }
 }
 
 
