54285e9e5ca9b858cd22283a10fa042a15dcd560
Andy Lester <andy@petdance.com>
2020-06-20 23:11:25 -0500

    Got the singing working

--- exercism/rust/beer-song/src/lib.rs
+++ exercism/rust/beer-song/src/lib.rs
@@ -6,16 +6,17 @@ fn inflect_bottles(n: u32) -> String {
     };
 }
 pub fn verse(n: u32) -> String {
-    let these = inflect_bottles(n);
-    let that = inflect_bottles(n-1);
-
     return match n {
         1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
         0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
-        _ => format!("{0} of beer on the wall, {0} of beer.\nTake one down and pass it around, {1} of beer on the wall.\n", these, that),
+        _ => format!("{0} of beer on the wall, {0} of beer.\nTake one down and pass it around, {1} of beer on the wall.\n", inflect_bottles(n), inflect_bottles(n-1)),
     };
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..=start).rev()
+        .map(verse)
+        .collect::<Vec<String>>()
+        .join("\n")
+        .to_string()
 }
