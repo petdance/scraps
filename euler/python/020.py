90223d61d470300d5d6c6ada34ee94466001116c
Andy Lester <andy@petdance.com>
2022-08-28 20:40:57 -0500

    Brute force

---
+++ euler/python/020.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def factorial(n):
+    if n == 1:
+        return 1
+
+    return n * factorial(n-1)
+
+
+n = factorial(100)
+s = f"{n}"
+
+print(sum([int(x) for x in s]))
