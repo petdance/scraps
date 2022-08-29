0de008603983b18ec1c4fcd9899d3d1ec510b780
Andy Lester <andy@petdance.com>
2022-08-28 20:27:35 -0500

    First working version on the small triangle

--- euler/python/018.py
+++ euler/python/018.py
@@ -10,7 +10,25 @@ TRIANGLE = '''
 
 def get_rows(triangle):
     """Breaks the triangle into an array of arrays of numbers"""
-    return [s.split(' ') for s in triangle.splitlines() if s != ""]
+    return [list(map(int,s.split(' '))) for s in triangle.splitlines() if s != ""]
+
+def best_path_starting_at(rows, row, col):
+    nextrow = row + 1
+    if nextrow >= len(rows):
+        return [rows[row][col]]
+
+    left = best_path_starting_at(rows, nextrow, col)
+    right = best_path_starting_at(rows, nextrow, col+1)
+
+    if sum(left) > sum(right):
+        best = left
+    else:
+        best = right
+
+    best.insert(0, rows[row][col])
+    return best
+
 
 rows = get_rows(TRIANGLE)
-print(rows)
+best = best_path_starting_at(rows, 0, 0)
+print(sum(best))
