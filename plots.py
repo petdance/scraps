0af40771cf208c848b58196dc89ca3a013ca91ce
Andy Lester <andy@petdance.com>
2022-10-26 22:50:24 -0500

    more fun

--- plots.py
+++ plots.py
@@ -4,10 +4,9 @@ from sympy import *
 from sympy.plotting import plot3d
 
 x,y = symbols('x y')
-f = 2 * x + 3 * y
-print(x)
-print(y)
-print(f)
+#f = 2 * x + 3 * y
+
+f = sqrt(abs(x*y))
 plot3d(f)
 
 
