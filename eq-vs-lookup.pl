24650835f74960efdc8fc059a4135a799b3bda82
Andy Lester <andy@petdance.com>
2022-12-11 00:27:38 -0600

    First version

---
+++ eq-vs-lookup.pl
@@ -0,0 +1,37 @@
+use 5.010;
+use strict;
+use warnings;
+use Benchmark qw'cmpthese';
+
+my %sections = (
+    somestring1 => 1,
+    somestring2 => 1,
+    somestring3 => 1,
+    somestring4 => 1
+);
+
+my @elements = map { 'somestring' . int(1 + rand(10)) } 1 .. 100;
+
+my $namespace;
+
+say "Perl version $^V";
+
+cmpthese(100000, {
+    hash_value => sub {
+        foreach my $element (@elements) {
+            $namespace = $element if $sections{$element};
+        }
+    },
+    hash_exists => sub {
+        foreach my $element (@elements) {
+            $namespace = $element if exists $sections{$element};
+        }
+    },
+    string_cmp => sub {
+        foreach my $element (@elements) {
+            $namespace = $element if (
+                $element eq'somestring3' ||
+                $element eq'somestring4');
+        }
+    },
+});
