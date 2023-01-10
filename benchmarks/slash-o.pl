c1ae753437f8fb03695ba5329bc0ed65aec0cfc5
Andy Lester <andy@petdance.com>
2023-01-10 15:09:27 -0600

    First version

---
+++ benchmarks/slash-o.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Benchmark ':all';
+
+my $COUNT = 10_000_000;
+
+say "$COUNT iterations under $^V";
+
+my $str = 'Who knows what evil lurks in the hearts of men?';
+my $pattern = 'in\s+the';
+
+cmpthese( $COUNT, {
+    match => sub {
+        return $str =~ /lurks $pattern/;
+    },
+    slasho => sub {
+        return $str =~ /lurks $pattern/o;
+    }
+} );
+
+
+exit 0;
