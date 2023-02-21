d3eb1ca6c9161071d8090b0a1303553fde1e79f5
Andy Lester <andy@petdance.com>
2023-02-21 00:10:37 -0600

    added hiddens for functions

--- benchmarks/render-hiddens.pl
+++ benchmarks/render-hiddens.pl
@@ -3,12 +3,37 @@
 use 5.010;
 use strict;
 use warnings;
+use experimental 'signatures';
 
 use Benchmark ':all';
+use HTML::Entities qw( encode_entities );
 
+use Carp::Assert::More;
 use Template;
 
+
+sub hidden_html( $name, $value ) {
+    return '<input type="hidden" name="' . encode_entities($name) . '" value="' . encode_entities($value // '') . '">' . "\n";
+}
+
+
+sub hiddens_html( $hiddens ) {
+    my $out = '';
+
+    if ( $hiddens ) { # Skips undefs and empty strings, which is how TT sends undefs.
+        # assert_hashref($hiddens);
+        for my $k ( sort keys %{$hiddens} ) {   # Keep rendering deterministic.
+            $out .= hidden_html( $k, $hiddens->{$k} );
+        }
+    }
+
+    return $out;
+}
+
 my $tt = Template->new(
+    VARIABLES => {
+        hiddens_html => \&hiddens_html,
+    }
 );
 
 
@@ -18,11 +43,16 @@ my $vars = {
     bat => undef,
 };
 
-my $COUNT = 50000;
+my $COUNT = 100000;
 
 cmpthese( $COUNT, {
     inline => sub {
         my $template = 'render-hiddens-inline.tt';
+        my $output;
+        $tt->process( $template, $vars, \$output ) or die $Template::ERROR;
+    },
+    function => sub {
+        my $template = 'render-hiddens-function.tt';
         my $output;
         $tt->process( $template, $vars, \$output ) or die $Template::ERROR;
     },
