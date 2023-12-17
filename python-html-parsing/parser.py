885fe08d5306b185a4500e3371d5590fae1b6577
Andy Lester <andy@petdance.com>
2023-12-17 11:41:30 -0600

    First version of parser

---
+++ python-html-parsing/parser.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+
+# https://docs.python.org/3/library/html.parser.html
+
+# Create a custom parser by subclassing HTMLParser
+class ImgFinder(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img':
+            for attr in attrs:
+                if attr[0] == 'src':
+                    print(attr[1])
+
+# Create an instance of your custom parser
+finder = ImgFinder()
+
+
+with open("sample.html") as f:
+    for line in f:
+        finder.feed(line)
